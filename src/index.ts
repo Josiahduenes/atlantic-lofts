8fb7a3690d045d342f4df37189e7b5bf28e29128
Josiahduenes <109304389+Josiahduenes@users.noreply.github.com>
2023-02-17 09:07:51 -0600

    Built Initial Calendar

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+import type { Event } from './type';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.querySelector<HTMLDivElement>('[data-element="calendar"]');
+  if (!calendarElement) return;
+
+  const events = getEvents();
+  console.log({ events });
+
+  const calendar = new Calendar(calendarElement, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek',
+    },
+
+    events,
+  });
+
+  calendar.render();
 });
+
+const getEvents = (): Event[] => {
+  const scripts = document.querySelectorAll('[data-element="event-data"]');
+  const events = [...scripts]
+    .map((script) => {
+      const event: Event = JSON.parse(script.textContent!);
+      event.start = new Date(event.start);
+      event.end = new Date(event.end);
+
+      return event;
+    })
+    .filter(Boolean);
+
+  return events;
+};
---
+++ src/type.ts
@@ -0,0 +1,7 @@
+export type Event = {
+  title: string;
+  start: string | Date;
+  end: string | Date;
+  details: string;
+  url: string;
+};
