a8ff781d9575fb2dc8e0f2736951b7d7762e6270
Josiahduenes <109304389+Josiahduenes@users.noreply.github.com>
2023-03-01 22:49:22 -0600

    Successful Data Ingetion
    Updated the script with working data pull from airtable to calendar.

--- src/index2.ts
+++ src/index2.ts
@@ -9,43 +9,43 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   const calendarElement = document.querySelector<HTMLDivElement>('[data-element="calendar"]');
   if (!calendarElement) return;
+  const apiKey =
+    'patjjOTvTCOCRglVs.aebd3d4f85a600cb5c228228069c341ce0f9a89ebff08f71e60fcb2c103a0adf';
+  const baseId = 'app6ak5FPSA3mu0KZ';
+  const tableName = 'tblUeZHD3igGLq1zi';
+  const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
+  let calendar;
+  let myData;
 
-  const events = getEvents();
-  console.log({ events });
-
-  const calendar = new Calendar(calendarElement, {
-    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,listWeek',
+  fetch(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
     },
-
-    events,
-  });
-
-  calendar.render();
-});
-
-const getEvents = (): Event[] => {
-  const scripts = document.querySelectorAll('[data-element="event-data"]');
-  const events = [...scripts]
-    .map((script) => {
-      const event: Event = JSON.parse(script.textContent!);
-      event.start = new Date(event.start);
-      event.end = new Date(event.end);
-
-      return event;
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const { records } = data; //Records = Full Data from AirTable.
+      const formattedData = records.map((record) => {
+        const { fields } = record;
+        return {
+          title: fields.Update_Name,
+          start: fields.Date,
+          end: fields.end_date,
+          url: fields.event_link,
+        };
+      });
+      myData = formattedData;
+      calendar = new Calendar(calendarElement, {
+        plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+        initialView: 'dayGridMonth',
+        events: myData,
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,listWeek',
+        },
+      });
+      calendar.render();
     })
-    .filter(Boolean);
-
-  return events;
-};
-
-// Set your Airtable API key and base ID
-// const apiKey = 'patjjOTvTCOCRglVs.cd518fa67be22cbed869a8c8a9ea9a71337ad24200a6ac72b53c5cf699df3f89';
-//const baseId = 'app6ak5FPSA3mu0KZ';
-
-// Set up the Airtable API endpoint URL
-// const url = `https://api.airtable.com/v0/${baseId}/tblUeZHD3igGLq1zi`
+    .catch((error) => console.error(error));
+});
