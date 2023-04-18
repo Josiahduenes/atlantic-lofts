0663bbb5dfbf740b0f5e699a1314416ba19b1223
Brett Miller <cypressbam@gmail.com>
2023-04-18 15:11:29 -0500

    Wized Custom Code Functionality - Email Load.

--- src/getUsers.ts
+++ src/getUsers.ts
@@ -1,25 +1,29 @@
-let emailList
+/* eslint-disable no-console */
 window.Webflow ||= [];
-window.Webflow.push(() => {
-    const apiKey = 'patjjOTvTCOCRglVs.aebd3d4f85a600cb5c228228069c341ce0f9a89ebff08f71e60fcb2c103a0adf';
+window.onload = async () => {
+  window.Webflow.push(() => {
+    console.log('YES IM WORKIN!');
+    const apiKey =
+      'patjjOTvTCOCRglVs.aebd3d4f85a600cb5c228228069c341ce0f9a89ebff08f71e60fcb2c103a0adf';
     const baseId = 'app6ak5FPSA3mu0KZ';
     const tableName = 'tblVr5AYC033ux66F';
     const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
     fetch(apiUrl, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-        },
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(async (data) => {
+        const { records } = data; //Records = Full Data from AirTable.
+        const formattedData = records.map((record) => {
+          const { fields } = record;
+          return {
+            email: fields.Email, //Format Data to "Email" : "<userEmail>". Used in custom Wized req.
+          };
+        });
+        await Wized.data.setVariable('memberemaillist', formattedData); //Set custom Wized variable "memberemaillist" to JSON Object of member emails from airtable.
       })
-        .then((response) => response.json())
-        .then((data) => {
-          const { records } = data; //Records = Full Data from AirTable.
-          const formattedData = records.map((record) => {
-            const { fields } = record;
-            return {
-              email: fields.Email
-            };
-          });
-          Wized.data.setVariable("memberemaillist",formattedData) //Set custom Wized variable "memberemaillist" to JSON Object of member emails from airtable.
-        })
-        .catch((error) => console.error(error));
-});
+      .catch((error) => console.error(error));
+  });
+};
